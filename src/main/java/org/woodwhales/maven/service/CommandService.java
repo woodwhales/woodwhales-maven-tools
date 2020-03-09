6cc1018541465d232a9ccc868069b9415e15b329
woodwhales <woodwhales@163.com>
2020-03-09 12:45:16 +0800

    updated generate command

--- src/main/java/org/woodwhales/maven/service/CommandService.java
+++ src/main/java/org/woodwhales/maven/service/CommandService.java
@@ -1,15 +1,15 @@
 package org.woodwhales.maven.service;
 
-import java.util.Objects;
-
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 import org.woodwhales.maven.dto.MavenDependencyInfo;
 
+import java.util.Objects;
+
 @Service
 public class CommandService {
 	
-	private String command = "mvn install:install-file -Dfile='%s' -DgroupId=%s -DartifactId=%s -Dversion=%s -Dpackaging=jar";
+	private String command = "mvn install:install-file -Dfile=\"%s\" -DgroupId=%s -DartifactId=%s -Dversion=%s -Dpackaging=jar";
 
 	public String buid(String jarFilePath, MavenDependencyInfo dependencyInfo) {
 		Objects.requireNonNull(dependencyInfo, "不允许maven坐标信息为空");
